3ea87666226ac935fb1d8d98ac5b287e48061bc7
Rafael Tomais <101938487+rafaeltomais@users.noreply.github.com>
2023-08-11 00:37:40 -0300

    remove files in resources folder

--- src/main/resources/import.sql
+++
@@ -1,8 +0,0 @@
-insert into usuario (id, login, user_name, password, should_notificate) values (1, 'rafaeltomais', 'rafael', '123', false);
-insert into usuario (id, login, user_name, password, should_notificate) values (2, 'lidiannetomais', 'lidianne', '123', false);
-insert into usuario (id, login, user_name, password, should_notificate) values (3, 'elvistomais', 'elvis', '123', false);
-
-insert into conta (due_day, description, due_value, payed, usuario_id) values (5, 'Mens. Seminario', '150', true, 1);
-insert into conta (due_day, description, due_value, payed, usuario_id) values (10, 'Energia', '120', false, 1);
-insert into conta (due_day, description, due_value, payed, usuario_id) values (20, 'Fatura Santander', '1000', false, 1);
-insert into conta (due_day, description, due_value, payed, usuario_id) values (1, 'Shein', '50', true, 2);
