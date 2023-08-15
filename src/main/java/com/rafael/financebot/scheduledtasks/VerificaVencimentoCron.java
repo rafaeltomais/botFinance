36856be23cb42026c35cb953b49951bb1fb4226a
Rafael Tomais <101938487+rafaeltomais@users.noreply.github.com>
2023-08-15 03:21:56 -0300

    Cron improve: messages and validations

--- src/main/java/com/rafael/financebot/scheduledtasks/VerificaVencimentoCron.java
+++ src/main/java/com/rafael/financebot/scheduledtasks/VerificaVencimentoCron.java
@@ -25,7 +25,6 @@ public class VerificaVencimentoCron {
         int diaDoMes = dataAtual.getDayOfMonth();
 
         List<Usuario> usuarioList = usuarioController.listarUsuarios();
-        StringBuilder mensagemVencimento = new StringBuilder("🚨🚨 Um alerta para você de vencimento! 🚨🚨\n\n");
 
         for(Usuario cadaUsuario : usuarioList) {
             Long identificador = cadaUsuario.getId();
@@ -33,17 +32,25 @@ public class VerificaVencimentoCron {
             List<Conta> contaList = cadaUsuario.getContasMensais();
 
             if(cadaUsuario.isShouldNotificate()) {
+                StringBuilder mensagemVencimento = new StringBuilder("🚨🚨 Um alerta para você de vencimento! 🚨🚨\n\n");
+                int quantidadeContaAberto = 0;
+
                 for (Conta cadaConta : contaList) {
                     int diaVencimento = cadaConta.getDueDay();
                     String descricaoConta = cadaConta.getDescription();
+                    boolean contaEmAberto = !cadaConta.isPayed();
+                    boolean isDiaVencimento = (diaDoMes) == diaVencimento;
+                    boolean isAmanhaVencimento = (diaDoMes + 1) == diaVencimento;
 
-                    if (diaVencimento == diaDoMes) {
-
-                        mensagemVencimento.append(String.format("Conta %s para o dia %d\n", descricaoConta, diaVencimento));
+                    if ((isDiaVencimento || isAmanhaVencimento) && contaEmAberto) {
+                        quantidadeContaAberto += 1;
+                        mensagemVencimento.append(String.format("Conta '%s' vence dia %d\n", descricaoConta, diaVencimento));
                     }
                 }
 
-                messageSender.sendMessage(chatId, String.valueOf(mensagemVencimento));
+                if(quantidadeContaAberto > 0){
+                    messageSender.sendMessage(chatId, String.valueOf(mensagemVencimento));
+                }
             }
         }
     }
